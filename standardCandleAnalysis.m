c4122e2c8abd35da1664b1a4fc8283531a05cdbf
Meghan Turner <meghanturner62@gmail.com>
2019-06-30 23:07:55 -0700

    Updates to standard candle analysis

--- standardCandleAnalysis.m
+++ standardCandleAnalysis.m
@@ -2,20 +2,27 @@ clear
 close all
 
 rawDynamicsData = 'E:\LocalEnrichment\Data\RawDynamicsData\';
-date = '2019-06-10\';
-project = '60mer-eGFP_invitro_invivoSettings_025umzStep7uW_01';
+date = '2019-06-10';
+project = '60mer-eGFP_invitro_injectionSettings_375Hz70uW';
 rawPath = [rawDynamicsData filesep date filesep project filesep];
 segPath = [rawDynamicsData filesep date filesep project filesep 'SegmentedImages' filesep];
+resultsPath = ['E:\Nick\LivemRNA\Dropbox\LocalEnrichmentResults\' date '_' project filesep];
+figPath = 'E:\Nick\LivemRNA\Dropbox\LocalEnrichmentFigures\calibration\';
+
+mkdir(resultsPath)
 
 tileScan = true;  %toggles between Time Series and Tile Scan settings
 showFigures = false;
-minRegionSize = 2;  %in 3D, mostly trying to get rid of hot pixels
+minRegionSize = 3;  %in 3D, mostly trying to get rid of hot pixels
 maxRegionSize = 70; %Arbitrary to avoid huge aggregates
 
 % Positioning limits - set based on max sizes in all 3D found below
 minXpos = 4;
 minYpos = 5;
 minZpos = 4;
+% Snip size
+xySnipRadius = 7;
+zSnipRadius = 4;
 
 fileList = dir([rawPath '*.tif']);
 
@@ -58,7 +65,9 @@ spotSnips = struct;
 % imshow(imageStruct(1).imStack(:,:,11),[])
 
 % Segment spots & fit 3D gaussians to them
-for im = 1:numel(imageStruct)
+h = waitbar(0, 'Finding & fitting spots');
+for im = 1:10%numel(imageStruct)
+    waitbar(im/numel(imageStruct), h, ['Finding & fitting spots in frame ' num2str(im) ' of ' num2str(numel(imageStruct))])
     imStackRaw = imageStruct(im).imStack;
     % Remove hot pixels and large aggregates
     imStackFilter = bwareaopen(imStackRaw > 0,minRegionSize,18);  %limiting connectivity so a series of diagonally connected hot pixels isn't considered an object
@@ -73,7 +82,7 @@ for im = 1:numel(imageStruct)
     yDim = size(imStackFilter,1);
     zDim = size(imStackFilter,3);  
     
-        % perform difference of gaussians filtering  
+    % Perform difference of gaussians filtering  
     imGauss1 = imgaussfilt3(imStackClean,1);
     imGauss5 = imgaussfilt3(imStackClean,5);
     imGaussDiff = imGauss1 - imGauss5;
@@ -87,10 +96,13 @@ for im = 1:numel(imageStruct)
     imStackFilt = imGaussDiff > thresh;
 %     imshowpair(max(imStackRaw,[],3),max(imStackFilt,[],3),'Scaling','independent')
 %     pause(1)
-
+    imStackNew = immultiply(imStackRaw, imStackFilt);
+%     imshow(max(imStackNew,[],3),[])
+%     pause(1)
     imStackLabel = bwconncomp(imStackFilt);
     imProps = regionprops3(imStackLabel,'Centroid','Solidity','Volume','VoxelList');
     
+    % Find the x, y, and z sizes of all the spots
     nZSliceVec = NaN(1,numel(imProps.VoxelList));
     nYPixVec = NaN(1,numel(imProps.VoxelList));
     nXPixVec = NaN(1,numel(imProps.VoxelList));
@@ -105,7 +117,8 @@ for im = 1:numel(imageStruct)
     maxXPix(im) = max(nXPixVec);
     maxYPix(im) = max(nYPixVec);
    
-    % Quality control, just position and minimum size for now
+    % Quality control, removing spots too close to the edge of the image 
+    % and spots that are too small
     zPosVec = imProps.Centroid(:,3);
     yPosVec = imProps.Centroid(:,1);
     xPosVec = imProps.Centroid(:,2);
@@ -119,27 +132,110 @@ for im = 1:numel(imageStruct)
     imPropsClean = imProps(keepIndices,:);
     % plot inferred spot centroids over top of original image
     centroidArray = vertcat(imPropsClean.Centroid);
-    showFigures = true;
     if showFigures
         figure(4);
-        imagesc(imProject);
+        imagesc(max(imStackNew,[],3));
         hold on
         scatter(centroidArray(:,1),centroidArray(:,2),20,'green')
+        hold off
     end
     
-    for k = size(centroidArray,1)
-        nSpots = nSpots + 1;
-        centroid = round(centroidArray(k,:));
-        xRange = centroid(2)-7:centroid(2)+7;
-        yRange = centroid(1)-7:centroid(1)+7;
-        zRange = centroid(3)-4:centroid(3)+4;
-        spotSnips(nSpots).Snip = imStackRaw(yRange,xRange,zRange);
+    if showFigures
+        figure(5);
+        imagesc(max(imStackNew,[],3));
+        hold on
+    end
+    % Fit a 3D gaussian to snips containing each spot
+    for k = 1:size(centroidArray,1)
+        yCentroid = round(centroidArray(k,1));
+        xCentroid = round(centroidArray(k,2));
+        zCentroid = round(centroidArray(k,3));
+        % Define a X x Y x Z pixel image around the centroid
+        yRange = (yCentroid - xySnipRadius):(yCentroid + xySnipRadius); 
+        xRange = (xCentroid - xySnipRadius):(xCentroid + xySnipRadius);
+        zRange = (zCentroid - zSnipRadius):(zCentroid + zSnipRadius); 
+        % Account for spots on the edge by trimming out of bounds indices
+        yRangeTrim = yRange(find(yRange > 0 & yRange <= yDim));
+        xRangeTrim = xRange(find(xRange > 0 & xRange <= xDim));
+        zRangeTrim = zRange(find(zRange > 0 & zRange <= zDim));
         
+        snipRaw = imStackRaw(xRangeTrim,yRangeTrim,zRangeTrim);    % Don't know why I needed to flip x & y here
+        snipSeg = imStackNew(xRangeTrim,yRangeTrim,zRangeTrim);    % Don't know why I needed to flip x & y here
+        snipRawProj = max(snipRaw,[],3);
+        snipSegProj = max(snipSeg,[],3);
+        snipFilt = snipSeg > 0;
+        
+        % Only save snips that contain a single spot (one labeled region)
+        if max(max(max(bwlabeln(snipFilt)))) == 1
+            nSpots = nSpots + 1;
+            spotSnips(nSpots).SnipRaw = snipRaw;
+            spotSnips(nSpots).SnipSegmented = snipSeg;
+            spotSnips(nSpots).Volume = imProps.Volume(k);
+            
+            % Plot snips to check
+            if showFigures
+                figure(5)
+                scatter(yCentroid,xCentroid,20,'green')
+
+                figure(6)
+                imagesc(snipRawProj)
+                spotTitle = ['spot number: ' num2str(nSpots)];
+                title(spotTitle)
+                drawnow
+                pause(0.5)
+            end
+            
+            % Fit 3D gaussians
+            gaussFitRaw = fit3DGaussianBeta(snipRaw);        
+            gaussFitSeg = fit3DGaussianBeta(snipSeg);
+            
+            spotSnips(nSpots).GaussFitRaw = gaussFitRaw;
+            spotSnips(nSpots).GaussFitSegmented = gaussFitSeg;
+        end
+    end
+    
+    if showFigures
+        figure(5)
+        hold off
     end
 end
-
+close(h);
 
 maxZ = max(maxZStack);
 maxX = max(maxXPix);
 maxY = max(maxYPix);
+
+%% 
+gaussAmp = NaN(1,numel(spotSnips));
+for i = 1:numel(spotSnips)
+    gaussFitCurr = [spotSnips(i).GaussFitRaw];
+%     if gaussFitCurr(1) >= 1 && spotSnips(i).Volume >= 5
+        gaussAmp(i) = gaussFitCurr(1);
+%     end
+end
+volume = [spotSnips.Volume];
+
+meanAmp = nanmean(gaussAmp);
+stdAmp = nanstd(gaussAmp);
+varAmp = nanvar(gaussAmp);
+
+spotSnipfile = [resultsPath project '.mat'];
+save(spotSnipfile,'spotSnips')
+
+histFig = figure(7);
+histogram(gaussAmp)
+xlabel('Amplitude of 3D Gaussian Fit (au)')
+ylabel('Count')
+title(project, 'Interpreter', 'none')
+legend(['mean = ' num2str(meanAmp) '; std = ' num2str(stdAmp)])
+% saveas(histFig,[figPath project '_hist.fig'])
+% saveas(histFig,[figPath project '_hist.png'])
+
+% Plotting to see if the really bright outliers are also the largest spots
+% ampVolScatterFig = figure(8);
+% scatter(gaussAmp, volume)
+% xlabel('Amplitude of 3D Gaussian Fit (au)')
+% ylabel('Volume of segmented spot (pixels)')
+% saveas(ampVolScatterFig,[figPath project '_ampVolScatter.fig'])
+% saveas(ampVolScatterFig,[figPath project '_ampVolScatter.png'])
 
