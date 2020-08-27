dfa76c3a4dffe7e4c1eda9bfef27077b82239878
Nicholas Lammers <nlammers@BERKELEY.EDU>
2020-08-27 11:02:30 -0700

    Tweaks to pipeline to be compatible with new data foramts

--- utilities/load_stacks.m
+++ utilities/load_stacks.m
@@ -1,8 +1,9 @@
-function stack = load_stacks(RawPath, src, frame, channel)
+function stack = load_stacks(RawPath, src, frame, channel,xDim,yDim,zDim)
 
-    stack = [];%NaN(size(mcp_stack));
-    files = dir([RawPath src '/*_' sprintf('%03d',frame) '*_ch0' num2str(channel) '.tif']);
-    for im = 2:numel(files)-1      
-        stack(:,:,im-1) = double(imread([RawPath src '/' files(im).name]));
-    end
-    
+%   stack = [];%NaN(size(mcp_stack));
+  file = dir([RawPath src '/*_' sprintf('%03d',frame) '*_ch0' num2str(channel) '.tif']);
+%     for im = 2:numel(files)-1      
+%         stack(:,:,im-1) = double(imread([RawPath src '/' files(im).name]));
+%     end
+  stack = imreadStack2([file(1).folder '/' file(1).name], yDim, xDim, zDim+2);
+  stack = stack(:,:,2:end-1);
