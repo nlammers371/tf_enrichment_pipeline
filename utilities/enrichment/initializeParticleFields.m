a4240d68e40d590c5b971e7277021f1a012da1b1
nlammers371 <nlammers@berkeley.edu>
2020-09-13 13:37:33 -0700

    Incorporated liveProject more fully into main01

--- utilities/enrichment/initializeParticleFields.m
+++ utilities/enrichment/initializeParticleFields.m
@@ -1,23 +1,23 @@
-function compiledSchnitzCells = initializeParticleFields(compiledSchnitzCells,sWithNC,...
-  n_entries)%,threeDFlag,APFlag,DetrendedZFlag)
+function compiledSchnitzCells = initializeParticleFields(compiledSchnitzCells,nucleusCounter,...
+  n_entries,has3DSpotInfo,hasAPInfo)
 
-    compiledSchnitzCells(sWithNC).particleID = NaN;
-    compiledSchnitzCells(sWithNC).xPosParticle = NaN(1,n_entries);
-    compiledSchnitzCells(sWithNC).yPosParticle = NaN(1,n_entries);
-    compiledSchnitzCells(sWithNC).zPosParticle = NaN(1,n_entries);      
-    compiledSchnitzCells(sWithNC).spotFrames = NaN(1,n_entries); 
-%     if threeDFlag
-      compiledSchnitzCells(sWithNC).xPosParticle3D = NaN(1,n_entries);
-      compiledSchnitzCells(sWithNC).yPosParticle3D = NaN(1,n_entries);
-      compiledSchnitzCells(sWithNC).zPosParticle3D = NaN(1,n_entries); 
-      compiledSchnitzCells(sWithNC).fluo3D = NaN(1,n_entries);
-%     end
-%     if APFlag 
-    compiledSchnitzCells(sWithNC).APPosParticle = NaN(1,n_entries);
-%     end
-%     if DetrendedZFlag
-    compiledSchnitzCells(sWithNC).zPosParticleDetrended = NaN(1,n_entries);
-%     end
-    compiledSchnitzCells(sWithNC).fluo = NaN(1,n_entries); 
-    compiledSchnitzCells(sWithNC).fluoOffset = NaN(1,n_entries); 
-    compiledSchnitzCells(sWithNC).spotFrames = NaN(1,n_entries); 
+    compiledSchnitzCells(nucleusCounter).particleID = NaN;
+    compiledSchnitzCells(nucleusCounter).xPosParticle = NaN(1,n_entries);
+    compiledSchnitzCells(nucleusCounter).yPosParticle = NaN(1,n_entries);
+    compiledSchnitzCells(nucleusCounter).zPosParticle = NaN(1,n_entries);      
+    compiledSchnitzCells(nucleusCounter).spotFrames = NaN(1,n_entries); 
+    if has3DSpotInfo
+      compiledSchnitzCells(nucleusCounter).xPosParticle3D = NaN(1,n_entries);
+      compiledSchnitzCells(nucleusCounter).yPosParticle3D = NaN(1,n_entries);
+      compiledSchnitzCells(nucleusCounter).zPosParticle3D = NaN(1,n_entries); 
+      compiledSchnitzCells(nucleusCounter).fluo3D = NaN(1,n_entries);
+    end
+    if hasAPInfo 
+      compiledSchnitzCells(nucleusCounter).APPosParticle = NaN(1,n_entries);
+    end
+
+    compiledSchnitzCells(nucleusCounter).zPosParticleDetrended = NaN(1,n_entries);
+
+    compiledSchnitzCells(nucleusCounter).fluo = NaN(1,n_entries); 
+    compiledSchnitzCells(nucleusCounter).fluoOffset = NaN(1,n_entries); 
+    compiledSchnitzCells(nucleusCounter).spotFrames = NaN(1,n_entries); 
