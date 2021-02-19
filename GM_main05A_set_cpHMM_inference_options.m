3ceaea87f4c637216667ee04f3ac0906ce02c75b
gdmartini <martini@berkeley.edu>
2021-02-18 18:05:45 -0800

    Creates GM script for running on savio

---
+++ GM_main05A_set_cpHMM_inference_options.m
@@ -0,0 +1,46 @@
+% Script to call primary cpHMM wrapper function
+
+clear
+close all
+warning('off','all') %Shut off Warnings
+addpath(genpath('utilities'))
+
+
+inferenceInfo = struct;
+
+% set project identifiers (only applicable if running this on savio)
+inferenceInfo.projectNameCell = {'hbBAC-MS2-25C'}; % {'2xDl-Ven_hbP2P-mCh'};
+
+% set inference options
+inferenceInfo.ProteinBinFlag = 0;
+inferenceInfo.FluoBinFlag = 0;
+%inferenceInfo.timeBins = {[0 60*10],[60*10 60*40]};
+timeBins = cell(1, 8);
+for i = 1:8
+    timeBins{i} = [((i-1)*5)*60, ((i-1)*5+15)*60];
+end
+inferenceInfo.timeBins = timeBins; % should be longer than 15min
+inferenceInfo.apBins = linspace(.15, .5, 15);%linspace(-.2,.2,10);
+
+% set core model specs
+inferenceInfo.modelSpecs.nStates = 2; %3; % number of states in system
+inferenceInfo.modelSpecs.nSteps = 4; % 7; % number of steps to traverse gene
+inferenceInfo.modelSpecs.alphaFrac =  1302/6444;%1275 / 4670;%
+
+% other info
+inferenceInfo.AdditionalGroupingVariable = '';%'Stripe'
+inferenceInfo.SampleSize = 4500;
+inferenceInfo.useQCFlag = true;
+
+% Get basic project info and determing file paths
+liveProject = LiveEnrichmentProject(inferenceInfo.projectNameCell{1});
+
+% save
+slashes = regexp(liveProject.dataPath,'/|\');
+dataDir = liveProject.dataPath(1:slashes(end-1));
+inferenceDir = [dataDir 'inferenceDirectory' filesep];
+mkdir(inferenceDir)
+save([inferenceDir 'inferenceInfo.mat'],'inferenceInfo')
+
+% copy bash file to inference directory
+copyfile('run_cpHMM.sh',[inferenceDir 'run_cpHMM.sh'])
