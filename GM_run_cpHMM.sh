5c58af90e2d12a708c18a28a510bf683a3abef9e
gdmartini <martini@berkeley.edu>
2021-03-02 17:17:35 -0800

    Update GM_run_cpHMM.sh

--- GM_run_cpHMM.sh
+++ GM_run_cpHMM.sh
@@ -21,10 +21,10 @@
 #SBATCH --ntasks-per-node=1
 #
 # Wall clock limit:
-#SBATCH --time=2:00:00
+#SBATCH --time=24:00:00
 #
 
-## NL: This tells Savio to run 10 distinct instances of the job (essentially euivalent to "nBoots")
+## NL: This tells Savio to run 10 distinct instances of the job (essentially equivalent to "nBoots")
 #SBATCH -a 1-10
 
 ## Command(s) to run:
