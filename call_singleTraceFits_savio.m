764321c3d212d238f05e32f5dea92d37b9672edc
gdmartini <martini@berkeley.edu>
2021-03-08 12:43:02 -0800

    savio batch script for running main06

--- call_singleTraceFits_savio.m
+++ call_singleTraceFits_savio.m
@@ -2,10 +2,10 @@
 clear 
 close all
 
-inferenceDir = pwd;
-inferenceDir = [inferenceDir, filesep];
+singleTraceFitsDir = pwd;
+singleTraceFitsDir = [singleTraceFitsDir, filesep];
 % move to inference directory
 cd('~/repos/tf_enrichment_pipeline/');
 
 % call the inference function
-GM_main05_conduct_cpHMM_inference(inferenceDir);
+GM_main06_incorporate_hmm_results(singleTraceFitsDir);
