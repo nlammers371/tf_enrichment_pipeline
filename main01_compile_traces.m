0cc5c0d42a43b1acb7283dfec10bf735a96dc755
Nicholas Lammers <nlammers@BERKELEY.EDU>
2021-08-26 09:42:32 -0700

    Assorted minor changes

--- main01_compile_traces.m
+++ main01_compile_traces.m
@@ -119,7 +119,8 @@ end
 
 [liveProject, numExperiments, dataName, hasAPInfo, has3DSpotInfo, hasProteinInfo] = headerFunction(projectName);
 %hasProteinInfo = false;
-has3DSpotInfo = false;
+hasProteinInfo = false;
+
 %% %%%%%%%%%%%%%%%%% Extract relevant processed data %%%%%%%%%%%%%%%%%%%%%%
 
 % Generate master structure with info on all nuclei and traces in
